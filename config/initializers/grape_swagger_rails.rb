3db608f5153cea0de81b61c767dabd4ecf88565c
Venky <venkatesh.n@dreamorbit.com>
2020-10-20 16:20:27 +0530

    Setup grape and swagger

---
+++ config/initializers/grape_swagger_rails.rb
@@ -0,0 +1,5 @@
+GrapeSwaggerRails.options.url      = "/api/v1/swagger_doc"
+GrapeSwaggerRails.options.app_name = 'Cybrilla'
+GrapeSwaggerRails.options.app_url  = ''
+GrapeSwaggerRails.options.api_key_name = 'Authorization'
+GrapeSwaggerRails.options.api_key_type = 'header'
